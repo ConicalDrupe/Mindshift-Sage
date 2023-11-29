ae8f4206d98044b06e92859de6542dbff8458a6b
ConicalDrupe <ChrisJacobBoon@gmail.com>
2023-11-28 21:14:04 -0500

    simplified document load. Added to-do's; create persistent db, improve character splitter, etc...

--- db_embedding.py
+++ db_embedding.py
@@ -4,7 +4,21 @@ from utils import download_and_load
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import CSVLoader
 
+### TO DO
+    # Create replace dowload and load, with simple langchain load
+    # Create local, persistent vector data base
+    # Improve text splitter / loader, think about data sets in the future
+    # add more data sets
+
+def load_quotes():
+    quotes_path = os.path.join(os.getcwd(),'data_store','quotes')
+    loader = CSVLoader(os.path.join(quotes_path,'philosopher-quotes.csv')) # Is there a way to store author and tags into metadata for textsplitter?
+    document = loader.load()
+    return document
+
+# The below needs tweeking - see TO DO above
 def query_quotes(query,k=3):
     """Connects to OpenAI embedding model, loads/splits documents into Chroma DB 
         then implements similarity search based on user query
