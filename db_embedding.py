baf2d032d7d51cce7ece3e54f727dc20fb62b4c4
ConicalDrupe <ChrisJacobBoon@gmail.com>
2023-12-02 16:11:54 -0500

    Simplified query function, compiled auxillary functions into utils

--- db_embedding.py
+++ db_embedding.py
@@ -1,10 +1,5 @@
-import os
-from open_ai_key import open_ai_key
-from utils import download_and_load 
-from langchain.embeddings.openai import OpenAIEmbeddings
+from utils import load_vectorstore 
 from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import CSVLoader
 
 ### TO DO
     # Create replace dowload and load, with simple langchain load
@@ -12,32 +7,36 @@ from langchain.document_loaders import CSVLoader
     # Improve text splitter / loader, think about data sets in the future
     # add more data sets
 
-def load_quotes():
-    quotes_path = os.path.join(os.getcwd(),'data_store','quotes')
-    loader = CSVLoader(os.path.join(quotes_path,'philosopher-quotes.csv')) # Is there a way to store author and tags into metadata for textsplitter?
-    document = loader.load()
-    return document
+
+def new_query_quotes(query,k=3):
+    # make sure database is build with data (verify)
+    # load_vector store to memory
+    # send query, with top k
+    # return response
+    db = load_vectorstore()
+    hits = db.similarity_search(query=query,k=k)
+    return hits
 
 # The below needs tweeking - see TO DO above
-def query_quotes(query,k=3):
-    """Connects to OpenAI embedding model, loads/splits documents into Chroma DB 
-        then implements similarity search based on user query
-    """
-    os.environ["OPENAI_API_KEY"] = open_ai_key
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-    
-    data_dir = os.path.join(os.getcwd(),'data_store','quotes')
-    docs = download_and_load(data_dir)
-    
-    # Lets split the document using Chracter splitting. 
-    splitter = CharacterTextSplitter(separator = "\n",
-                                    chunk_size=700, 
-                                    chunk_overlap=0,
-                                    length_function=len)
-    documents = splitter.split_documents(docs)
-    
-    chromadb = Chroma.from_documents(documents, embeddings) 
-    
-    results = chromadb.similarity_search(query,k=k)
-
-    return results
+# def query_quotes(query,k=3):
+#     """Connects to OpenAI embedding model, loads/splits documents into Chroma DB 
+#         then implements similarity search based on user query
+#     """
+#     os.environ["OPENAI_API_KEY"] = open_ai_key
+#     embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+#     
+#     data_dir = os.path.join(os.getcwd(),'data_store','quotes')
+#     docs = download_and_load(data_dir)
+#     
+#     # Lets split the document using Chracter splitting. 
+#     splitter = CharacterTextSplitter(separator = "\n",
+#                                     chunk_size=700, 
+#                                     chunk_overlap=0,
+#                                     length_function=len)
+#     documents = splitter.split_documents(docs)
+#     
+#     chromadb = Chroma.from_documents(documents, embeddings) 
+#     
+#     results = chromadb.similarity_search(query,k=k)
+#
+#     return results
--- utils.py
+++ utils.py
@@ -1,34 +1,16 @@
-from huggingface_hub import hf_hub_download
-from langchain.document_loaders import CSVLoader
 import os
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
 
-def download_and_load(download_path,repo_id='datastax/philosopher-quotes',filename='philosopher-quotes.csv',repo_type='dataset', check=False):
-    """Input download path,HF repoid,filename,type, and ruturns CSV loaded document"""
-    target_dir = os.path.join(os.getcwd(),'data_store',download_path)
-    if len(os.listdir(target_dir)) == 0: #checking if target path is empty
-        hf_hub_download(repo_id=repo_id, filename=filename,repo_type=repo_type,local_dir=download_path)
-        print("New files located in ")
-        for (root, dirs, file) in os.walk(download_path,topdown=True):
-            print(root)
-            print(dirs)
-            print(file)
-            print("---------------Next-Root-----------------")
+def load_vectorstore():
+    """ Make sure to run build.py first, to instantiate and create embeddings """
+    hf_embedding=HuggingFaceEmbeddings(model='all-MiniLM-L12-v2')
+    db_dir = os.path.join(os.getcwd(),'data_store','db')
+    db = Chroma(persist_directory=db_dir, embedding_function = hf_embedding)
+    return db
 
-    else:
-        pass
-        #print("Nothing Downloaded, file already exists")
-
-    # Load with CSVLoader
-    loader = CSVLoader(os.path.join(target_dir,'philosopher-quotes.csv')) # Is there a way to store author and tags into metadata for textsplitter?
-    document = loader.load()
-
-    if check:
-        doc_len = len(document)
-        print(f"Document contains {doc_len} docs")
-        for i, doc in enumerate(document):
-            print(f"Page {i} page content ", doc.page_content)
-            print(f"Page {i} metadata ", doc.metadata)
-            if i == 2: break
-        pass
-
-    return document
+def add_to_vectorstore(db,split_documents):
+    """ Pass in db from load_vectorstore """
+    db.add(split_documents)
+    print("Add complete")
+    return
